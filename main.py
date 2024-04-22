09c71dd1ff097a3818ae4c00c009d6fe82a16ad8
jalbrekt85 <jcalbrecht85@gmail.com>
2024-04-22 18:17:00 -0500

    Revert "feat: core logic, tx builder"
    This reverts commit d4c35a39a6e0a8bdd3208c000d971422ae8410ee.

--- main.py
+++ main.py
@@ -1,33 +1,5 @@
-import os
-import argparse
-
-from web3 import Web3
-from dotenv import load_dotenv
-
-from recycler.processing_pipeline import recycle_bribes
-from recycler.tx_builder.builder import build_tx
-
-
-load_dotenv()
-
-module_dir = os.path.dirname(os.path.abspath(__file__))
-output_dir = os.path.join(module_dir, "output")
-
-RECYCLE_PCT = 0.7
-
-
 def main() -> None:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("amount", type=int, help="USDC to recycle")
-    usdc_amount = int(parser.parse_args().amount * 1e6)
-
-    web3 = Web3(Web3.HTTPProvider(os.getenv("ETHNODEURL")))
-
-    recycle_usdc_amount = int(usdc_amount * RECYCLE_PCT)
-    dao_usdc_amount = int(usdc_amount - recycle_usdc_amount)
-
-    bribe_amounts = recycle_bribes(web3, recycle_usdc_amount)
-    build_tx(dao_usdc_amount, bribe_amounts, output_dir)
+    pass
 
 
 if __name__ == "__main__":
